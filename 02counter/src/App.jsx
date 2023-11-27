11bc4bb5e165d843801681cac445321970a89011
arbaj0612 <junaid.786mulani@gmail.com>
2023-11-27 19:58:22 +0530

    Counter Project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  const [count , setCount] = useState(0)
+
+  const addValue = () => {
+    setCount(count + 1)
+    if(count == 20){
+      setCount(count)
+    }
+    
+  }
+
+  const removeValue = () => {
+    setCount(count - 1)
+    if(count == 0) {
+      setCount(count)
+    }
+  }
+
+  return (
+    <>
+      <h1>Counter Project</h1>
+      <h3>Counter Value = {count}</h3>
+      <button onClick={addValue}>Add Value {count}</button>
+      <br/>
+      <button onClick={removeValue}>Remove Value {count}</button>
+    </>
+  )
+}
+
+export default App
